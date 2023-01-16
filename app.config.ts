d547168f811b3d458fa994628cf169ba90810b9b
PowerDev9512 <>
2023-01-16 18:47:22 +1100

    Configure eas auto update

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,12 @@
 import { ConfigContext, ExpoConfig } from "@expo/config";
+
 const IS_DEV = process.env.APP_VARIANT === "development";
 const IS_BETA = process.env.APP_VARIANT === "beta";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: IS_DEV ? `[DEV] ${config.name}` : config.name,
-  slug: config.slug,
+  name: IS_DEV ? `[DEV] ${config.name}` : config.name ?? "FitnessTracker",
+  slug: config.slug ?? "FitnessTracker",
   icon: IS_DEV
     ? "./src/assets/icons/icon-dev.png"
     : IS_BETA
@@ -13,6 +14,13 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     : "./src/assets/icons/icon.png",
   android: {
     ...config.android,
+    runtimeVersion: "1.0.0",
     package: IS_DEV ? "dev.tamagui.expo.devclient" : "dev.tamagui.expo.client",
+  },
+  ios: {
+    ...config.ios,
+    runtimeVersion: {
+      policy: "sdkVersion",
+    },
   },
 });
