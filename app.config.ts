5582c393ce854e8705621006b2668daaf6fb6bff
PowerDev9512 <>
2023-03-23 21:54:18 +1100

    Add prod endpoint instead of ngrok one oops

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,9 @@ const IS_BETA = process.env.APP_VARIANT === "beta";
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   jsEngine: "hermes",
+  updates: {
+    url: "https://u.expo.dev/87b6b9bf-d4ba-40c3-83af-a358273cf05a",
+  },
   name: IS_DEV ? `[DEV] ${config.name}` : config.name ?? "FitnessTracker",
   slug: config.slug ?? "FitnessTracker",
   icon: IS_DEV
--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const local = "https://b23c-106-71-191-213.au.ngrok.io/";
+const local = "http://13.238.79.62/";
 
 export const client = axios.create({
   baseURL: local,
